55ec7e2518fe80674c476fa9f46a8c9a4202a1e7
YounesBoukhatem <younes.tv.56@gmail.com>
2026-03-31 20:18:22 -0400

    :fix: fixed server issue

--- server.js
+++ server.js
@@ -7,14 +7,14 @@ var http = require("http");
 var url = require("url");
 
 function start(route, handle) {
+  function onRequest(request, response) {
+    var pathname = url.parse(request.url).pathname;
+    console.log("Request for " + pathname + " received.");
+    route(handle, pathname, response, request);
+  }
 
-    function onRequest(request, response) {
-        var pathname = url.parse(request.url).pathname;
-        console.log("Request for " + pathname + " received.");
-        route(handle, pathname, response, request);
-    }
-
-    http.createServer(onRequest).listen(8080);
+  const PORT = process.env.PORT || 8080;
+  http.createServer(onRequest).listen(PORT);
 }
 
 exports.start = start;
